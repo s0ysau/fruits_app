8004b975484c19fae3ee43ccd715236bafc64126
jerricklc@gmail.com <jerricklc@gmail.com>
2022-10-24 17:41:22 -0400

    more stuff was added after week 12 & week 13

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,22 +1,36 @@
 // Start our router
 const express = require('express') // import express
-// only the router none of the other app stuff 
+// only the router none of the other app stuff
 const router = express.Router()
 // Need the model bc the job of the model is to give us access to the DB
 const dataController = require('./dataController')
 const viewController = require('./viewController')
+const apiController = require('./apiController')
 
-// Route
+// API Routes
+// Index
+router.get('/api', dataController.index, apiController.index)
+// Delete
+router.delete('/api/:id', dataController.destroy, apiController.show)
+// Update
+router.put('/api/:id', dataController.update, apiController.show)
+// Create
+router.post('/api', dataController.create, apiController.show)
+// Show
+router.get('/api/:id', dataController.show, apiController.show)
+
+
+// Non API Route
 // Index
 router.get('/', dataController.index, viewController.index)
 // New
-router.get('/new', viewController.newView )
+router.get('/new', viewController.newView)
 // delete
 router.delete('/:id', dataController.destroy, viewController.redirectHome)
 // update
 router.put('/:id', dataController.update, viewController.redirectShow)
 // create
-router.post('/', dataController.create, viewController.redirectShow) 
+router.post('/', dataController.create, viewController.redirectShow)
 // edit
 router.get('/:id/edit', dataController.show, viewController.edit)
 // show
--- server.js
+++ server.js
@@ -6,25 +6,31 @@ const db = require('./models/db')
 const app = express()
 
 // Configure the app (app.set)
-/*Start Config */
+/* Start Config */
+// Creates res.locals.data
 app.use(express.urlencoded({ extended: true })) // This code makes us have req.body
+app.use(express.json())
 app.use((req, res, next) => {
-    res.locals.data = {}
-    next()
+  res.locals.data = {}
+  next()
 })
+
+/* Start Middleware */
 app.engine('jsx', require('jsx-view-engine').createEngine())
 app.set('view engine', 'jsx') // register the jsx view engine
 db.once('open', () => {
-    console.log('connected to MongoDB Atlas')
+  console.log('connected to MongoDB Atlas')
 })
 
-/*Start Middleware */
-app.use(methodOverride('_method'))
+
+app.use(methodOverride('_method')) // Allows override method
 app.use(express.static('public'))
+
+//setting up /fruits as the entry 
 app.use('/fruits', require('./controllers/routeController'))
 /* END Middleware */
 
 // Tell the app to listen on a port
 app.listen(3000, () => {
-    console.log('Listening on Port 3000')
+  console.log('Listening on Port 3000')
 })
